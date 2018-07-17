3491a2cdb6c1c52ee5f1ed0ed0735f82161a8eda
Eric LE DEVEDEC <ledevedec.eric@gmail.com>
2018-07-17 15:09:09 +0200

    add doctrine - Person entity - Person crud methode - WelcomeController

---
+++ src/Form/PersonType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Person;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phoneNumber')
+            ->add('email')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Person::class,
+        ]);
+    }
+}
